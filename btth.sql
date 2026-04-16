40c9285be0e033192887c36886e311e53db88d50
daonguyenlamhoang-art <nguyenlonghoang302710@gmail.com>
2026-04-16 21:32:27 +0700

    Add files via upload

--- btth.sql
+++ btth.sql
@@ -1,19 +1,32 @@
-CREATE DATABASE LibraryDB;
-USE LibraryDB;
-
-CREATE TABLE BOOK (
-    MaSach CHAR(5) PRIMARY KEY,
-    TenSach VARCHAR(200) NOT NULL,
-    SoLuong INT CHECK (SoLuong >= 0),
-    GiaThue DECIMAL(10,2) DEFAULT 5000
+CREATE TABLE products (
+    ProductID VARCHAR(10) PRIMARY KEY,
+    ProductName VARCHAR(100),
+    Size VARCHAR(10),
+    Price DECIMAL(10,2)
 );
 
-ALTER TABLE BOOK
-ADD NgayNhap DATE;
+INSERT INTO products (ProductID, ProductName, Size, Price)
+VALUES
+('P01', 'Áo sơ mi trắng', 'L', 250000),
+('P02', 'Quần Jean xanh', 'M', 450000),
+('P03', 'Áo thun Basic', 'XL', 150000),
+('P04', 'Áo hoodie', NULL, -200000);
 
-CREATE TABLE BORROW_BOOKS (
-    MaMuon INT AUTO_INCREMENT PRIMARY KEY,
-    MaSach CHAR(5),
-    NgayMuon DATE DEFAULT CURRENT_DATE,
-    FOREIGN KEY (MaSach) REFERENCES BOOK(MaSach)
-);
+UPDATE products
+SET Price = 400000
+WHERE ProductID = 'P02';
+
+UPDATE products
+SET Price = Price * 1.1;
+
+DELETE FROM products
+WHERE ProductID = 'P03';
+
+SELECT * FROM products;
+
+SELECT ProductName, Size
+FROM products;
+
+SELECT *
+FROM products
+WHERE Price > 300000;
